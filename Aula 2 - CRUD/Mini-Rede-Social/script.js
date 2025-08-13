8591d39835d98498bfa0097a77c83e812ce7270d
Arthur Berlofa Bosi <berlofabosi.arthur@gmail.com>
2025-08-12 22:35:07 -0300

    Aula 02 - 2sem

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,51 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function (){
+    mostrarPosts();
+
+    document.querySelector("#postForm").addEventListener('submit', addPost);
+}
+
+
+function addPost(infosDoEvento){
+    infosDoEvento.preventDefault();
+
+    const textoPost = document.querySelector("#postText").value;
+    const categoriaPost = document.querySelector("#postCategory").value;
+    const imagePost = document.querySelector("#postImage").value;
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagePost,
+        date: new Date().toLocaleDateString()
+    }
+
+    posts.unshift(novoPost);
+
+    mostrarPosts();
+}
+
+
+function mostrarPosts(){
+    // Pega o elemento que terá a lista de posts no html
+    const listaPosts = document.querySelector("#postList"); 
+    
+    // Passa por cada item do array, criando um card de post
+    posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div");
+        
+        cardPost.innerHTML = `
+            <h2>${pegaItem.text}</h2>
+            ${pegaItem.image? `<img src= ${pegaItem.image} />` : ""}
+            <p><b>Categoria: </b>${pegaItem.category}</p>
+            <p><b>Data e Hora: </b>${pegaItem.date}</p>
+            <button>Editar</button>
+            <button>Apagar</button>
+        `;
+        // Adiciona o post na lista de posts no HTML
+        listaPosts.append(cardPost);
+    })
+}
